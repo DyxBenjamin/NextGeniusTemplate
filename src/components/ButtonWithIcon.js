6544982dc1c8f00ea60cb357dd97476a2b2cd8a9
Diego_5160 <diego.islas@qrsof.com>
2023-04-12 00:31:30 -0600

    Button and swipeable carousel

---
+++ src/components/ButtonWithIcon.js
@@ -0,0 +1,26 @@
+import {Button} from '@mui/material';
+import LocalDiningIcon from '@mui/icons-material/LocalDining';
+
+const ButtonWithIcon = () => {
+    return (
+        <Button
+            sx={{
+                display: 'block',
+                mx: 'auto',
+                mb: 5,
+                width: '80vw',
+                height: 120,
+                borderRadius: 5,
+                bgcolor: 'gray',
+                color: 'white',
+                '&:hover': {
+                    bgcolor: 'darkGray',
+                },
+            }}
+            startIcon={<LocalDiningIcon/>}>
+            Crear mi menú del día
+        </Button>
+    );
+};
+
+export default ButtonWithIcon;
--- src/pages/app/index.js
+++ src/pages/app/index.js
@@ -1,11 +1,40 @@
 import {Fragment} from "react";
+import SwipeableCarousel from "@components/SwipeableCarousel";
+import {Box, Button} from "@mui/material";
+import ButtonWithIcon from "@components/ButtonWithIcon";
+import Container from "@mui/material/Container";
 
+const cards = [
+    {
+        title: 'Card 1',
+        img: 'https://source.unsplash.com/random/400x300',
+    },
+    {
+        title: 'Card 2',
+        img: 'https://source.unsplash.com/random/401x300',
+    },
+    {
+        title: 'Card 3',
+        img: 'https://source.unsplash.com/random/402x300',
+    },
+];
 
 export default function App() {
 
     return (
-        <Fragment>
-            Index
-        </Fragment>
+        <Box
+            sx={{
+                display: "flex",
+                flexGrow: 1,
+                alignItems: "stretch",
+                minHeight: "100vh"
+            }}>
+            <Container
+                maxWidth="xl"
+                sx={{mt: 2, display: "flex", flexDirection: "column"}}>
+                <ButtonWithIcon />
+                <SwipeableCarousel cards={cards}/>
+            </Container>
+        </Box>
     )
 }
