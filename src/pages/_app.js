3a400c0f667089430706a8725b500da2f6a17549
Diego_5160 <diego.islas@qrsof.com>
2023-04-11 23:25:14 -0600

    adding layout for app version

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
 import '@styles/main.scss'
+import Layout from "@components/layout";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({Component, pageProps, router}) {
+    if (router.pathname.startsWith('/app')) {
+        return (
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        );
+    }
+    return <Component {...pageProps} />;
 }
