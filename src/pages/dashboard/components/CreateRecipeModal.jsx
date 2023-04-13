f643c0575c90b51fb78e42ba90d67e1e5543b75f
Benjamín Garrido <bengarr.negocios@gmail.com>
2023-04-12 22:32:21 -0600

    Create recipe modal

--- src/pages/dashboard/components/CreateRecipeModal.jsx
+++ src/pages/dashboard/components/CreateRecipeModal.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import {Box, Button, Modal, Typography} from "@mui/material";
 
+
+const styles = {
+	modal: {
+		position: 'absolute',
+		top: '50%',
+		left: '50%',
+		transform: 'translate(-50%, -50%)',
+		width: 400,
+		bgcolor: 'background.paper',
+		border: '2px solid #000',
+		boxShadow: 24,
+		p: 4,
+	}
+}
+
 export default function CreateRecipeModal({open, handleClose, data}) {
 	return (<>
 			{ data &&
-				<Modal open={open} onClose={handleClose}>
-					<Box>
+				<Modal
+					open={open}
+					onClose={handleClose}  >
+					<Box 					sx={styles.modal}
+					>
 						<Typography variant={'h6'}>
 							Crear receta
 						</Typography>
